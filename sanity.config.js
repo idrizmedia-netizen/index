00e850cc4638acab9deac95227e299af5e873184
idrizmedia-netizen <idrizmedia@gmail.com>
2026-05-15 16:08:58 +0500

    Create sanity.config.js

---
+++ sanity.config.js
@@ -0,0 +1,29 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Ziyomap Admin',
+
+  projectId: '25lh4m7u',
+  dataset: 'production',
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: [
+      {
+        name: 'post',
+        type: 'document',
+        title: 'Maqolalar',
+        fields: [
+          { name: 'title', type: 'string', title: 'Sarlavha' },
+          { name: 'category', type: 'string', title: 'Kategoriya' },
+          { name: 'preview', type: 'text', title: 'Qisqa ta\'rif' },
+          { name: 'telegramLink', type: 'url', title: 'Telegram Havolasi' },
+          { name: 'mainImage', type: 'image', title: 'Muqova rasmi' }
+        ]
+      }
+    ],
+  },
+})
