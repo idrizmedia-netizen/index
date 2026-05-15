54ac7400a3c8ef2ea370bfc696d563d7ea94d8e7
idrizmedia-netizen <idrizmedia@gmail.com>
2026-05-15 16:07:40 +0500

    Create sanity.cli.js

---
+++ sanity.cli.js
@@ -0,0 +1,10 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '25lh4m7u',
+    dataset: 'production'
+  },
+  // Tashkilot ID-sini saqlash (ixtiyoriy, lekin foydali)
+  organizationId: 'onIr8G39c',
+})
