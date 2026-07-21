bd568144b943a5b3fe36e64fc27095618937fa02
idrizmedia-netizen <idrizmedia@gmail.com>
2026-07-22 00:02:06 +0500

    Add files via upload

---
+++ js/hududlar.js
@@ -0,0 +1,79 @@
+// O'zbekiston Respublikasi ma'muriy-hududiy bo'linishi (viloyat -> tuman/shahar ro'yxati)
+// Ro'yxatdan o'tish formasidagi "Viloyat" va "Tuman/shahar" tanlov (select) maydonlari uchun ishlatiladi.
+window.UZ_HUDUDLAR = {
+    "Qoraqalpog'iston Respublikasi": [
+        "Nukus shahri", "Amudaryo tumani", "Beruniy tumani", "Chimboy tumani", "Ellikqal'a tumani",
+        "Kegeyli tumani", "Mo'ynoq tumani", "Nukus tumani", "Qanliko'l tumani", "Qorao'zak tumani",
+        "Qo'ng'irot tumani", "Shumanay tumani", "Taxtako'pir tumani", "To'rtko'l tumani", "Xo'jayli tumani",
+    ],
+    "Andijon viloyati": [
+        "Andijon shahri", "Xonobod shahri", "Andijon tumani", "Asaka tumani", "Baliqchi tumani",
+        "Bo'z tumani", "Buloqboshi tumani", "Izboskan tumani", "Jalaquduq tumani", "Xo'jaobod tumani",
+        "Qo'rg'ontepa tumani", "Marhamat tumani", "Oltinko'l tumani", "Paxtaobod tumani",
+        "Shahrixon tumani", "Ulug'nor tumani",
+    ],
+    "Buxoro viloyati": [
+        "Buxoro shahri", "Kogon shahri", "Buxoro tumani", "G'ijduvon tumani", "Jondor tumani",
+        "Kogon tumani", "Olot tumani", "Peshku tumani", "Qorako'l tumani", "Qorovulbozor tumani",
+        "Romitan tumani", "Shofirkon tumani", "Vobkent tumani",
+    ],
+    "Farg'ona viloyati": [
+        "Farg'ona shahri", "Qo'qon shahri", "Marg'ilon shahri", "Bag'dod tumani", "Beshariq tumani",
+        "Buvayda tumani", "Dang'ara tumani", "Farg'ona tumani", "Furqat tumani", "Oltiariq tumani",
+        "Qo'shtepa tumani", "Quva tumani", "Rishton tumani", "So'x tumani", "Toshloq tumani",
+        "Uchko'prik tumani", "O'zbekiston tumani", "Yozyovon tumani",
+    ],
+    "Jizzax viloyati": [
+        "Jizzax shahri", "Arnasoy tumani", "Baxmal tumani", "Do'stlik tumani", "Forish tumani",
+        "G'allaorol tumani", "Mirzachul tumani", "Paxtakor tumani", "Yangiobod tumani",
+        "Zafarobod tumani", "Zarbdor tumani", "Zomin tumani",
+    ],
+    "Xorazm viloyati": [
+        "Urganch shahri", "Xiva shahri", "Bog'ot tumani", "Gurlan tumani", "Xazorasp tumani",
+        "Xonqa tumani", "Qo'shko'pir tumani", "Shovot tumani", "Urganch tumani",
+        "Yangiariq tumani", "Yangibozor tumani",
+    ],
+    "Namangan viloyati": [
+        "Namangan shahri", "Chortoq tumani", "Chust tumani", "Kosonsoy tumani", "Mingbuloq tumani",
+        "Namangan tumani", "Norin tumani", "Pop tumani", "To'raqo'rg'on tumani", "Uychi tumani",
+        "Uchqo'rg'on tumani", "Yangiqo'rg'on tumani", "Davlatobod tumani",
+    ],
+    "Navoiy viloyati": [
+        "Navoiy shahri", "Zarafshon shahri", "Konimex tumani", "Karmana tumani", "Navbahor tumani",
+        "Nurota tumani", "Qiziltepa tumani", "Tomdi tumani", "Uchquduq tumani", "Xatirchi tumani",
+    ],
+    "Qashqadaryo viloyati": [
+        "Qarshi shahri", "Shahrisabz shahri", "Chiroqchi tumani", "Dehqonobod tumani", "G'uzor tumani",
+        "Kasbi tumani", "Kitob tumani", "Koson tumani", "Mirishkor tumani", "Muborak tumani",
+        "Nishon tumani", "Qamashi tumani", "Qarshi tumani", "Shahrisabz tumani", "Yakkabog' tumani",
+    ],
+    "Samarqand viloyati": [
+        "Samarqand shahri", "Kattaqo'rg'on shahri", "Bulung'ur tumani", "Ishtixon tumani", "Jomboy tumani",
+        "Kattaqo'rg'on tumani", "Qo'shrabot tumani", "Narpay tumani", "Nurobod tumani", "Oqdaryo tumani",
+        "Payariq tumani", "Paxtachi tumani", "Pastdarg'om tumani", "Samarqand tumani",
+        "Tayloq tumani", "Urgut tumani",
+    ],
+    "Sirdaryo viloyati": [
+        "Guliston shahri", "Yangiyer shahri", "Shirin shahri", "Boyovut tumani", "Guliston tumani",
+        "Xovos tumani", "Mirzaobod tumani", "Oqoltin tumani", "Sardoba tumani",
+        "Sayxunobod tumani", "Sirdaryo tumani",
+    ],
+    "Surxondaryo viloyati": [
+        "Termiz shahri", "Angor tumani", "Bandixon tumani", "Boysun tumani", "Denov tumani",
+        "Jarqo'rg'on tumani", "Muzrabot tumani", "Oltinsoy tumani", "Qiziriq tumani",
+        "Qumqo'rg'on tumani", "Sariosiyo tumani", "Sherobod tumani", "Sho'rchi tumani",
+        "Termiz tumani", "Uzun tumani",
+    ],
+    "Toshkent shahri": [
+        "Bektemir tumani", "Chilonzor tumani", "Mirzo Ulug'bek tumani", "Mirobod tumani",
+        "Sergeli tumani", "Shayxontohur tumani", "Olmazor tumani", "Uchtepa tumani",
+        "Yakkasaroy tumani", "Yunusobod tumani", "Yashnobod tumani", "Yangihayot tumani",
+    ],
+    "Toshkent viloyati": [
+        "Angren shahri", "Bekobod shahri", "Chirchiq shahri", "Olmaliq shahri", "Yangiyo'l shahri",
+        "Nurafshon shahri", "Bekobod tumani", "Bo'ka tumani", "Bo'stonliq tumani", "Chinoz tumani",
+        "Qibray tumani", "Ohangaron tumani", "Oqqo'rg'on tumani", "Parkent tumani", "Piskent tumani",
+        "Quyichirchiq tumani", "Toshkent tumani", "Urtachirchiq tumani", "Yangiyo'l tumani",
+        "Yuqorichirchiq tumani", "Zangiota tumani",
+    ],
+};
